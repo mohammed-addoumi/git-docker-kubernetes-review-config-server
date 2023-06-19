aa72f210c7bfd087ca36bee498b3eed0cad9431e
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-06-19 10:19:43 +0100

    init project

---
+++ src/main/java/org/allreview/gitdockerkubernetesreviewconfigserver/GitDockerKubernetesReviewConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.allreview.gitdockerkubernetesreviewconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDockerKubernetesReviewConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDockerKubernetesReviewConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/allreview/gitdockerkubernetesreviewconfigserver/GitDockerKubernetesReviewConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.allreview.gitdockerkubernetesreviewconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDockerKubernetesReviewConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
